377377ca8c2fd5e9a8bc22c30aea66cc5419fac8
UtkinE <y_ne@bk.ru>
2015-04-05 18:13:34 +0700

    worked

--- src/main/java/net/eutkin/data/controller/ShowController.java
+++ src/main/java/net/eutkin/data/controller/ShowController.java
@@ -1,14 +1,17 @@
 package net.eutkin.data.controller;
 
+import net.eutkin.data.entity.DataTS;
 import net.eutkin.data.service.IDataMensService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @Controller
 public class ShowController extends AbstractController {
@@ -16,11 +19,12 @@ public class ShowController extends AbstractController {
     @Autowired
     private IDataMensService dataMensService;
 
-    @RequestMapping("/show")
-    protected ModelAndView handleRequestInternal(HttpServletRequest request,
+    @RequestMapping(value="/show", method = RequestMethod.GET)
+    public ModelAndView handleRequestInternal(HttpServletRequest request,
                                                  HttpServletResponse response)
             throws Exception {
-        ModelAndView modelAndView = new ModelAndView();
+        ModelAndView modelAndView = new ModelAndView("show");
+        List<DataTS> list = dataMensService.showAllData();
         try {
             modelAndView.addObject("dataMensList", dataMensService.showAllData());
         }
